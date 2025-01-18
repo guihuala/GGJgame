f7d7bf2785c71d1272a0c337292c7e9af841283d
sfstgskg <1105910996@qq.com>
2025-01-19 02:22:59 +0800

    长按消除气泡

--- 3 - real_new_project/Bubble/base_bubble.gd
+++ 3 - real_new_project/Bubble/base_bubble.gd
@@ -14,10 +14,24 @@ var is_in_area: bool = false
 @export var min_salary: int = 10
 @export var max_salary: int = 50
 
+@export var probabilty: float
+var is_long_press_bubble: bool = false
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var button: Button = $Button
+
 func _ready():
 	# 配置刚体属性
 	mass = bubble_mass
 	gravity_scale = bubble_gravity
+	
+	animated_sprite_2d.visible = false
+	button.button_down.connect(_on_button_button_down)
+	button.button_up.connect(_on_button_button_up)
+	
+	if randf() < probabilty:
+		animated_sprite_2d.visible = true
+		is_long_press_bubble = true
 
 # 设置对话框文本
 func set_text(text: String):
@@ -25,6 +39,11 @@ func set_text(text: String):
 
 # 销毁气泡
 func on_destroy_bubble() -> void:
+	# 生成随机薪水
+	var random_salary = randi_range(min_salary, max_salary)
+	
+	# 增加随机数额的薪水
+	GameManager.increase_salary(random_salary)
 	# 查找场景中第一个 character 组的节点
 	var character = get_tree().get_nodes_in_group("Character")[0]
 	
@@ -38,20 +57,24 @@ func on_destroy_bubble() -> void:
 	# 销毁气泡
 	queue_free()
 
-# 按钮点击事件
-func _on_button_pressed() -> void:
-	# 生成随机薪水
-	var random_salary = randi_range(min_salary, max_salary)
-	
-	# 增加随机数额的薪水
-	GameManager.increase_salary(random_salary)
-	
-	# 播放销毁特效
-	on_destroy_bubble()
+## 按钮点击事件
+#func _on_button_pressed() -> void:
+	## 生成随机薪水
+	#var random_salary = randi_range(min_salary, max_salary)
+	#
+	## 增加随机数额的薪水
+	#GameManager.increase_salary(random_salary)
+	#
+	## 播放销毁特效
+	#on_destroy_bubble()
 
 func _on_button_button_down() -> void:
-	pass # Replace with function body.
-
+	if is_long_press_bubble:
+		animated_sprite_2d.play("loading")
+		await animated_sprite_2d.animation_finished
+		on_destroy_bubble()
+	else:
+		on_destroy_bubble()
 
 func _on_button_button_up() -> void:
-	pass # Replace with function body.
+	animated_sprite_2d.stop()
