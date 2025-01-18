6129a16723bb42028d03eee88f2621a56102530f
sfstgskg <1105910996@qq.com>
2025-01-18 16:39:22 +0800

    不知道改了点啥

--- 3 - real_new_project/Bubble/base_bubble.gd
+++ 3 - real_new_project/Bubble/base_bubble.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 # 对话内容节点
-@onready var text_label: Label = $Label
+@onready var text_label: Label = $PanelContainer/Label
 # 鼠标是否在气泡的判断区域内，如果在就可以戳破
 var is_in_area: bool = false
 
@@ -19,7 +19,7 @@ func set_text(text: String):
 	text_label.text = text
 
 func on_destroy_bubble() -> void:
-	AudioManager.play_sfx("bubble")
+	#AudioManager.play_sfx("bubble")
 	queue_free()
 
 func _on_button_pressed() -> void:
